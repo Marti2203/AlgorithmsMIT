e44189115f0ea4c2294a1f37872d0b0972b01fcd
marti_2203 <marti_2203@abv.bg>
2017-07-03 17:05:11 +0300

    First Commit

---
+++ InsertionSort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define swap(x,y) do{(x)^=(y); (y)^=(x); (x)^=(y);}while(0);
+#define and &&
+#define printArr(arr,start,size) for(int i=0;i<size;i++)printf("%i ",testArr[i]); printf("\n");
+#define printInt(a) printf("%i\n",a);
+#define Size 10
+int identity(int a){return a;}
+
+void InsertionSort(int* arr,int size, int (*compare)(int,int))
+{
+    for(int j=1;j<size;j++)
+    {
+        int key=arr[j],i=j-1;
+        while(i>-1 and compare(arr[i],key))
+            {
+                arr[i+1]=arr[i]; 
+                i--;
+            }
+
+        arr[i+1]=key;
+    }
+}
+
+int descending(int a,int b){return a<b;}
+int ascending(int a,int b){return a>b;}
+
+//#define printArr(arr,size) printArr(arr,0,size)
+
+int main()
+{
+    int* testArr=malloc(Size*sizeof(int));
+    testArr[0]=6;
+    testArr[1]=8;
+    testArr[2]=3;
+    testArr[3]=19;
+    testArr[4]=100;
+    testArr[5]=84;
+    testArr[6]=222;
+    testArr[7]=11;
+    testArr[8]=60;
+    testArr[9]=333;
+    //printArr(testArr,0,Size)
+
+    InsertionSort(testArr,Size,ascending);
+    printArr(testArr,0,Size)
+    return 0;
+}
